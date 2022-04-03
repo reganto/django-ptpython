2c7bbfdf20e130c7e912e36b4add26a56d9724cf
Morteza Naghizadeh <tell.reganto@gmail.com>
2022-04-03 15:18:53 +0430

    Black reformat

--- django_ptpython/apps.py
+++ django_ptpython/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class DjangoPtpythonConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_ptpython'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_ptpython"
