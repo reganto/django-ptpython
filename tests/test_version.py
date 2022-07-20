135576ddc7d94845f753b573cdbac6de03ef9816
Reganto <tell.reganto@gmail.com>
2022-07-20 10:21:30 +0430

    :construction_worker: Fixed #4 -- Added Django 3.2.14 LTS Support

--- tests/test_version.py
+++ tests/test_version.py
@@ -5,4 +5,4 @@ from django_ptpython import __version__
 
 class TestVersion(unittest.TestCase):
     def test_django_ptpython_version(self):
-        assert __version__ == "1.0.0"
+        assert __version__ == "1.0.2"
